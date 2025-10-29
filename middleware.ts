760d9be7accd6bc391fc988fe51c57929714733e
Leo Ware <leoware@uni.minerva.edu>
2025-10-29 14:18:10 -0400

    auth updates

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,11 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+    authorizedParties: [
+        'https://returntofreedom.org',
+        'https://rtf.leoware.io',
+    ],
+});
 
 export const config = {
   matcher: [
